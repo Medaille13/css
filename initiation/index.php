4887a03a7c57d9d8668c310003997d9537e08518
Medaille13 <110163029+Medaille13@users.noreply.github.com>
2024-01-30 07:32:33 +0100

    update the README.md with link to contributing guide

---
+++ initiation/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap" rel="stylesheet">
+    <link href="./style2/style.css<?php echo '?v='.time(); ?>" rel="stylesheet" type="text/css" />
+    <title>Initiation</title>
+</head>
+<body>
+    <section class="e">
+    <article class="card">
+    <h1>H1 HTML Ipsum Presents</h1>
+    
+        <p class="bloc_retreci">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.<strong> Donec eu libero sit amet</strong> quam egestas
+        semper. Aenean ultricies mi vitae est.Mauris placerat eleifend leo. Quisque sit amet est et sapien
+        ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi.
+        Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.<a
+        href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+        
+    <h2>H2 Header Level 2</h2>
+        
+        <ol class="enum">
+            <li>Lorem ipsum dolor sit amet,consectetuer<span class="souligne">adipiscing elit</span>.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+        </ol>
+        
+        
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a
+                est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est
+                malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p>
+       
+
+
+    <h3>H3 Header Level 3</h3>
+            
+        <ul class="enume">
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+        </ul>
+            
+            
+    <h2>H2 Header Level 2</h2>
+
+        <p class="paragra-phe">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam,
+            feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
+            mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat
+            wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci,<br>
+            sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.
+            Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam                erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <ol>
+                <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+                <li>Aliquam tincidunt mauris eu risus.</li>
+                <li>Vestibulum auctor dapibus neque.</li>
+            </ol>       
+</article>                   
+</section>                   
+</body>
+</html>
