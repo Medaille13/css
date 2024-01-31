0447398d703ee68f68a5bafe4b8a2868b452f824
Medaille13 <110163029+Medaille13@users.noreply.github.com>
2024-01-31 08:52:48 +0100

    fini

--- initiation/index.php
+++ initiation/index.php
@@ -19,18 +19,19 @@
                 Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.<strong> Donec eu libero sit amet</strong> quam egestas
                 semper. Aenean ultricies mi vitae est.Mauris placerat eleifend leo. Quisque sit amet est et sapien
                 ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi.
-                Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.<a
+                Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a
                 href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
         
              <h2>H2 Header Level 2</h2>
-                
-                <ol class="enumeration">
-                    <li>Lorem ipsum dolor sit amet,consectetuer<span class="souligne">adipiscing elit</span>.</li>
+                <div class="enumeration">
+                <ol>
+                    <li>Lorem ipsum dolor sit amet,consectetuer adipiscing elit.</li>
                     <li>Aliquam tincidunt mauris eu risus.</li>
                 </ol>
+                </div>
                 
         
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a
+                <p class="bloc_retreci_deux"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a
                     est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est
                     malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p>
         
@@ -41,25 +42,25 @@
                     <li>Aliquam tincidunt mauris eu risus.</li>
                 </ul>
             
-             <h2>H2 Header Level 2</h2>
+             <h2 class="second_hdeux">H2 Header Level 2</h2>
 
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam,
+                <p class="dernier_paragraphe">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam,
                     feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies
                     mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat
                     wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
                     ipsum rutrum orci,<br>
                     sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.
                     Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquamerat 
-                    volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p><br>
+                    volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
                         
-                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p><br>
-                    
-                    <ol class="enumeration_deux">
+                <p class="dernier_paragraphe">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                <div class="enumeration_deux">
+                    <ol>
                         <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
                         <li>Aliquam tincidunt mauris eu risus.</li>
                         <li>Vestibulum auctor dapibus neque.</li>
                     </ol>       
-
+                </div>
             </article>                   
         </section>                   
     </body>
